ba85a7863b17cab60fbb45c99e0f2450db87ea9f
DongGyu Jung <bang2brew@gmail.com>
2023-02-28 23:28:34 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,11 @@
+/*
+ *
+    - 공간을 둘 이상 등록 -> "헤비 유저"
+*/
+SELECT ID, NAME, HOST_ID
+FROM PLACES
+WHERE HOST_ID IN (SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(*) >= 2)
+ORDER BY ID
